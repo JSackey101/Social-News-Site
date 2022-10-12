c368cc2872279655d5d7723ec475d908629fc5f5
Chris Owen <chris@sigmalabs.xyz>
2022-10-12 14:08:09 +0100

    Update news_scaper.py

--- backend/news_scaper.py
+++ backend/news_scaper.py
@@ -8,6 +8,13 @@ def get_html(url):
   html = html_bytes.decode('utf_8')
   return html_doc
 
+def parse_stories_bs(domain_url, html):
+  stories = []
+  soup = BeautifulSoup(html, 'html.parser')
+  # TO DO
+
+  return stories
+
 if __name__ == "__main__":
 	bbc_url = "http://bbc.co.uk"
 	bbc_html_doc = get_html(bbc_url)
