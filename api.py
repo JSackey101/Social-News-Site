3a792f827a0acd916c8546881158e42d7f633b42
Dan Keefe <peritract@hotmail.co.uk>
2023-06-26 15:32:22 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,33 @@
 from flask import Flask, current_app, jsonify
 
+stories = [
+    {
+        "id": 0,
+        "title": "Abdul Sharifu Was Buying Milk For A Neighbor's Baby. A Snowstorm Killed Him. ",
+        "url": "https://www.buzzfeednews.com/article/albertsamaha/abdul-sharifu-buffalo-blizzard-2022"
+    },
+    {
+        "id": 1,
+        "title": "Amazon Warehouse Worker Daniel Olayiwola Decided To Make A Podcast About Amazon's Working Conditions",
+        "url": "https://www.buzzfeednews.com/article/albertsamaha/daniel-olayiwola-amazon-scamazon-podcast"
+    },
+    {
+        "id": 2,
+        "title": "Eight People Share What It's Like To Live On $100,000 A Year",
+        "url": "https://www.buzzfeednews.com/article/venessawong/six-figure-salary-100k-a-year"
+    },
+    {
+        "id": 3,
+        "title": "Biden's Student Loan Forgiveness Plan Is Now On Hold. If You Applied, Tell Us What Your Plans Are.",
+        "url": "https://www.buzzfeednews.com/article/venessawong/bidens-student-loan-forgiveness-plan-is-now-on-hold-if-you"
+    },
+    {
+        "id": 4,
+        "title": "How A Group Of Dancers Sparked A Unionization Effort At A Los Angeles Strip Club",
+        "url": "https://www.buzzfeednews.com/article/otilliasteadman/strippers-unionizing-star-garden"
+    }
+]
+
 app = Flask(__name__)
 
 
