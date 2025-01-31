a872dbab82f780769f73d682f2ced635803a4c12
JSackey101 <jeffsackey101@gmail.com>
2025-01-31 12:42:33 +0000

    Updated get_html and parse_stories_bs.

--- news_scraper.py
+++ news_scraper.py
@@ -4,20 +4,30 @@ from bs4 import BeautifulSoup
 
 
 def get_html(url):
+    """ Get HTML from a URL. """
     page = urlopen(url)
     html_bytes = page.read()
-    html = html_bytes.decode("utf_8")
+    html_doc = html_bytes.decode("utf_8")
     return html_doc
 
 
 def parse_stories_bs(domain_url, html):
+    """ Create a list of story dictionaries containing title and url for input HTML. """
     stories = []
     soup = BeautifulSoup(html, "html.parser")
-    # TO DO
-
-    return stories
+    stories = soup.css.select(".e1vyq2e80")
+    story_list = []
+    for story in stories:
+        story_dict = {}
+        story_dict['title'] = str(story.find('p'))[
+            75:].split('<', maxsplit=1)[0]
+        story_dict['url'] = domain_url + str(story.find("a")['href'])
+        story_list.append(story_dict)
+    return story_list
 
 
 if __name__ == "__main__":
-    bbc_url = "http://bbc.co.uk"
+    BBC_URL = "http://bbc.co.uk"
     bbc_html_doc = get_html(bbc_url)
+    parse_stories_bs(domain_url=bbc_url, html=bbc_html_doc)
+    
