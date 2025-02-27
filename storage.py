c7b64667b3ccb71b89172410a193782e81c03bc1
JSackey101 <jeffsackey101@gmail.com>
2025-02-27 18:09:44 +0000

    Updated the save_to_file and load_from_file functions.

--- storage.py
+++ storage.py
@@ -1,8 +1,18 @@
-def save_to_file(data: ...) -> ...:
+""" Contains functions to handle the read/write operations. """
+
+import os
+import json
+
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+
+def save_to_file(data: list[dict]) -> None:
     """Save the data to a file called stories.json"""
-    ...
+    with open(os.path.join(ABS_PATH, data), mode="w", encoding="UTF-8") as f:
+        json.dump(data, f, indent=3)
 
 
-def load_from_file(data: ...) -> ...:
+def load_from_file() -> list[dict]:
     """Load the stories from a file called stories.json"""
-    ...
+    with open(os.path.join(ABS_PATH, "stories.json"), encoding="UTF-8") as file:
+        data = json.load(file)
+        return data
